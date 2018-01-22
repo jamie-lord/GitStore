f0d06dd2e78c4f209450c9edf08f6ac4b0791b49
Jamie Lord <me@jamielord.co.uk>
2018-01-22 21:37:04 +0000

    Working save and get methods for generic git json storage

---
+++ GitStore.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using Newtonsoft.Json;
+
+namespace GitStore
+{
+    public class GitStore
+    {
+        private readonly string _repoDirectory;
+        private readonly string _name;
+        private readonly string _email;
+
+        public GitStore(string repoDirectory, string name, string email)
+        {
+            _repoDirectory = repoDirectory;
+            _name = name;
+            _email = email;
+
+            Repository.Init(_repoDirectory);
+        }
+
+        public async Task Save<T>(T obj)
+        {
+            var json = ToJson(obj);
+            var objId = GetIdValue(obj);
+            var path = PathFor<T>(objId);
+
+            await File.WriteAllTextAsync(path, json);
+
+            using (var repo = new Repository(_repoDirectory))
+            {
+                Commands.Stage(repo, path);
+
+                if (!repo.RetrieveStatus().IsDirty)
+                {
+                    return;
+                }
+
+                var signature = new Signature(_name, _email, DateTime.Now);
+                try
+                {
+                    repo.Commit($"Added object of type {obj.GetType()} with id {objId}", signature, signature, new CommitOptions { PrettifyMessage = true });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public async Task<T> Get<T>(object objId)
+        {
+            var path = PathFor<T>(objId);
+            if (File.Exists(path))
+            {
+                return await ToObject<T>(path);
+            }
+            return default(T);
+        }
+
+        private string PathFor<T>(object objId)
+        {
+            return $"{_repoDirectory}/{objId}_{typeof(T)}.json";
+        }
+
+        private string ToJson<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented);
+        }
+
+        private async Task<T> ToObject<T>(string path)
+        {
+            var s = await File.ReadAllTextAsync(path);
+
+            if (!string.IsNullOrEmpty(s))
+            {
+                try
+                {
+                    return JsonConvert.DeserializeObject<T>(s);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return default(T);
+        }
+
+        private object GetIdValue<T>(T obj)
+        {
+            var props = obj.GetType().GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(ObjectId)));
+
+            if (props.Count() != 1)
+            {
+                return null;
+            }
+
+            return props.First().GetValue(obj);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ObjectId : Attribute
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace GitStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var store = new GitStore(@"/Users/jamie/Downloads/Test repo", "GitStore test", "test@test.com");
+
+            for (int i = 0; i < 100; i++)
+            {
+                var obj = new TestObject
+                {
+                    Id = i,
+                    Text = Path.GetRandomFileName()
+                };
+
+                store.Save(obj).Wait();
+
+                var tObj = store.Get<TestObject>(obj.Id).Result;
+            }
+        }
+    }
+
+    public class TestObject
+    {
+        [ObjectId]
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
